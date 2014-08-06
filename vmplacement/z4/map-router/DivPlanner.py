6996b76d4efe202d2682431c4413a0674472f157
Hui Song <hui.song@scss.tcd.ie>
2014-08-06 17:02:12 +0200

    color and shuffles

--- vmplacement/z4/map-router/DivPlanner.py
+++ vmplacement/z4/map-router/DivPlanner.py
@@ -8,6 +8,7 @@ from z3 import *
 from softz3_opt import *
 from softz3_diagnose import *
 from random import randint
+from random import choice
 from time import clock
 import unittest
 from painter import *
@@ -16,30 +17,35 @@ class Test(unittest.TestCase):
 
     def testName(self):
         
-        AlgoT, (algoT_pol, algoT_traf, algoT_goog) = EnumSort('AlgoT', ['JustPoll','PollOrTraff','Goog'])
+        AlgoT, (algoT_pol, algoT_traf, algoT_goog) = EnumSort('AlgoT', ['JustPoll','PollOrTraff', 'Goog'])
         EncT, (encT_pol, encT_traf) = EnumSort('EncT',['PollEnc', 'TraffEnc'])
         VmT, (vmT_ec2, vmT_azure) = EnumSort('VmT', ['EC2', 'Azure'])
+        StorT, (storT_app, storT_pltf) = EnumSort('StorT', ['StorageApp', 'StoragePlatform'])
         
         
-        
-        
-        AlgoI, algoI = EnumSort('AlgoI', ['algo1','algo2','algo3', 'algo4', 'algonew'])
-        EncI, encI = EnumSort('EncI', ['encnull','enc1', 'enc2'])
-        VmI, vmI = EnumSort('VmI', ['vmnull', 'vm1', 'vmnew'])
+        AlgoI, algoI = EnumSort('AlgoI', ['algo0', 'algo1','algo2','algo3', 'algo4', 'algo5', 'algo6', 'algo7'])
+        EncI, encI = EnumSort('EncI', ['encnull','enc1', 'enc2', 'enc3'])
+        VmI, vmI = EnumSort('VmI', ['vmnull', 'vm1', 'vm2', 'vmnew'])
+        StorI, storI = EnumSort('StorI', ['stornull', 'stor1', 'stor2'])
         
         toAlgo = Function('toAlgo', AlgoI, AlgoT)
         toEnc = Function('toEnc', EncI, EncT)
         toVm = Function('toVm', VmI, VmT)
+        toStor = Function('toStor', StorI, StorT)
         
         dAlgoEnc = Function('dAlgoEnc', AlgoI, EncI)
         dAlgoVm = Function('dAlgoVm', AlgoI, VmI)
         dEncVm = Function('dEncVm', EncI, VmI)
+        dStorVm = Function('dStorVm', StorI, VmI)
+        dAlgoStor = Function('dAlgoStor', AlgoI, StorI)
         
         useAlgo = Function('useAlgo', AlgoI, BoolSort())
         useEnc = Function('useEnc', EncI, BoolSort())
         useVm = Function('useVM', VmI, BoolSort())
+        useStor = Function('useStor', StorI, BoolSort())
         
         memVm = Function('memVM', VmI, IntSort())
+        cacheAlgo = Function('cacheAlgo', AlgoI, BoolSort())
         
         iAlgo = Const('iAlgo', AlgoI)
         iiAlgo = Const('iiAlgo', AlgoI)
@@ -47,27 +53,36 @@ class Test(unittest.TestCase):
         iiEnc = Const('iiEnc', EncI)
         iVm = Const('iVm', VmI)
         iiVm = Const('iiVm', VmI)
+        iStor = Const('iStore', StorI)
         
         solver = SoftSolverOpt()
         
         #functional dependencies
-        solver.hard.append(
+        solver.add_hard(
             ForAll([iAlgo], Implies(useAlgo(iAlgo),
                         And(
                             Implies(toAlgo(iAlgo)==algoT_pol, toEnc(dAlgoEnc(iAlgo)) == encT_pol),
                             Implies(toAlgo(iAlgo)==algoT_traf, Or(toEnc(dAlgoEnc(iAlgo))== encT_pol, toEnc(dAlgoEnc(iAlgo))== encT_traf)),
-                            Implies(toAlgo(iAlgo)==algoT_goog, And(dAlgoEnc(iAlgo)==encI[0],dAlgoVm(iAlgo)==vmI[0])),
-                            Implies(Or(toAlgo(iAlgo)==algoT_pol, toAlgo(iAlgo)==algoT_traf), And(useVm(dAlgoVm(iAlgo)), useEnc(dAlgoEnc(iAlgo))))
+                            Implies(toAlgo(iAlgo)==algoT_goog, And(dAlgoEnc(iAlgo)==encI[0],dAlgoVm(iAlgo)==vmI[0],Not(cacheAlgo(iAlgo)))),
+                            Implies(Or(toAlgo(iAlgo)==algoT_pol, toAlgo(iAlgo)==algoT_traf), And(useVm(dAlgoVm(iAlgo)), useEnc(dAlgoEnc(iAlgo)))),
+                            Implies(cacheAlgo(iAlgo), useStor(dAlgoStor(iAlgo)))
                         )
                       )            
                   )
         )
         
-        solver.hard.append(
+        solver.add_hard(
             ForAll([iEnc], 
                 useEnc(iEnc) == useVm(dEncVm(iEnc))
             )
         )
+        
+        solver.add_hard(ForAll([iStor],
+                               Implies(And(useStor(iStor), toStor(iStor)==storT_app),
+                                       useVm(dStorVm(iStor))
+                                       )
+                               )
+                        )
         
         
         
@@ -79,21 +94,48 @@ class Test(unittest.TestCase):
                                )
                         )
         
-        solver.hard.append(And([useAlgo(algo) for algo in algoI]))
+
+
+        
+        solver.add_hard(Not(useAlgo(algoI[0])))
+        solver.add_hard(Not(useVm(vmI[0])))
+        solver.add_hard(Not(useEnc(encI[0])))
+        solver.add_hard(Not(useStor(storI[0])))
+        
+        #VM capacity: suppose each app needs 1 GB of memory
+        solver.add_hard(ForAll([iVm],
+                               Sum(
+                                   [If(And(useAlgo(a), dAlgoVm(a)==iVm),1,0) for a in algoI] +
+                                   [If(And(useEnc(a), dEncVm(a)==iVm),1,0) for a in encI] +
+                                   [If(And(useStor(a), dStorVm(a)==iVm),1,0) for a in storI]
+                                   )
+                               <= memVm(iVm)
+                               )
+                        )
+        
+        #requirements
+        solver.hard.append(And([useAlgo(algo) for algo in algoI[1:]]))
+        
+        #solver.add_hard(And([cacheAlgo(algoI[0]), cacheAlgo(algoI[1]),cacheAlgo(algoI[2]),cacheAlgo(algoI[3])]))
+        for a in algoI:
+            solver.add_soft(cacheAlgo(a), 12)
         #solver.add_hard(toAlgo(algoI[1])==algoT_goog)
-        solver.add_hard(toAlgo(algoI[0])==algoT_pol)
-        solver.hard.append(Not(useVm(vmI[0])))
-        solver.hard.append(Not(useEnc(encI[0])))
+        #solver.add_hard(toAlgo(algoI[0])==algoT_pol)
         
-        
-        
-        
-        solver.add_soft(toVm(vmI[1])==vmT_ec2,10)
+        #solver.add_soft(toVm(vmI[1])==vmT_ec2,10)
         solver.add_soft(Not(useVm(vmI[2])),10)
-        solver.add_soft(memVm(vmI[1])==5, 20)
-        solver.add_soft(toVm(vmI[1])!=toVm(vmI[2]),10)
-        solver.add_soft(toAlgo(algoI[1])!=toAlgo(algoI[3]),10)
-        solver.add_soft(dAlgoVm(algoI[1])!=dAlgoVm(algoI[2]),10)
+        solver.add_soft(memVm(vmI[1])==2, 50)
+        solver.add_soft(memVm(vmI[2])==4, 50)
+        solver.add_soft(memVm(vmI[3])==2, 50)
+        
+        
+        
+        #perturbations
+        solver.add_soft(Not(useVm(vmI[3])),30)
+        #solver.add_soft(toVm(vmI[1])!=toVm(vmI[2]),10)
+        #solver.add_soft(toAlgo(algoI[1])!=toAlgo(algoI[3]),10)
+        #solver.add_soft(dAlgoVm(algoI[1])!=dAlgoVm(algoI[2]),10)
+        #solver.add_soft(dAlgoStor(algoI[1])!=dAlgoStor(algoI[2]),15)
         solver.init_solver()
         
         print len(solver.soft)
@@ -101,29 +143,62 @@ class Test(unittest.TestCase):
         solver.debug = False
         
         
-        print "final total: %d" % solver.search()
-            
-        
-        
+        print "final total: %d" % solver.search() 
         eval = solver.model().eval
         
-        print eval
         print eval(dEncVm(encI[1]))
         print eval(dAlgoVm(algoI[0]))
         print eval(useEnc(encI[1]))
+        print eval(dAlgoStor(algoI[1]))
         
         rp = ResultPainter()
-        rp.vars = algoI + encI + vmI
-        rp.refs = [dAlgoEnc, dAlgoVm, dEncVm]
-        rp.types = [toAlgo, toEnc, toVm]
-        rp.filters = [useAlgo, useEnc, useVm]
-        rp.attrs = [memVm]
+        rp.vars = algoI + encI + vmI + storI
+        rp.refs = [dAlgoEnc, dAlgoVm, dEncVm, dStorVm, dAlgoStor]
+        rp.types = [toAlgo, toEnc, toVm, toStor]
+        rp.filters = [useAlgo, useEnc, useVm, useStor]
+        rp.attrs = [memVm, cacheAlgo]
         rp.use_filters = True
+        
+        
+        print eval(dEncVm(encI[1]))
+        print eval(dAlgoVm(algoI[0]))
+        print eval(useEnc(encI[1]))
+        print eval(dAlgoStor(algoI[1]))
+        print 'use stor1: ' + str(eval(useStor(storI[1])))
+        print eval(dStorVm(storI[1]))
+        
         rp.eval = eval
         rp.make_graph()
+        
+        var_type = [(algoI, toAlgo), (encI, toEnc), (vmI, toVm), (storI, toStor)]
+        
+        for i in range(0,10):
+            for j in range(0,10):
+                if self.shuffle(solver, var_type): break
+            solver.init_solver()
+            print solver.search()
+            rp.eval = solver.model().eval
+            print rp._shannon()
+            rp.make_graph()
+        
+        
         #print eval(countA)  
-       
+        print rp._shannon()
  
+ 
+    def shuffle(self, solver, target):
+        (vars, type) = choice(target)
+        i1 = randint(1,len(vars)-2)
+        i2 = randint(i1+1, len(vars)-1)
+        v1 = vars[i1]
+        v2 = vars[i2]
+        print (v1, v2)
+        if str(solver.model().eval(type(v1)==type(v2)))=='False':
+            print "no use shuffle"
+            return False
+        solver.add_soft(type(v1)!=type(v2), 20)
+        return True
+        
 
         
         
--- vmplacement/z4/map-router/painter.py
+++ vmplacement/z4/map-router/painter.py
@@ -7,6 +7,8 @@ Created on 12 Nov 2013
 from z3 import *
 import networkx as nx
 import matplotlib.pyplot as plt
+import math
+import random
 
 
 class ResultPainter():
@@ -20,26 +22,43 @@ class ResultPainter():
         self.eval = None
         self.G = nx.Graph()
         self.use_filters = False
+        self._colors = ['#99aaee', '#eeaa99', '#ccffdd', '#ddffcc', '#aa99ee', 'g', 'r', 'm', 'y', 'c']
         
     def make_graph(self): 
-        for v in self.vars:
-            if self._filt(v):
-                self.G.add_node(str(v))
+        self.G.clear()
+        using_vars = [v for v in self.vars if self._filt(v)]
+        for v in using_vars:
+            type = self._type_of_v(v)
+            attrs = self._attr_of_v(v)
+            attrs['type'] = type
+            self.G.add_node(str(v),attrs)
         all_nodes_str = [str(n) for n in self.G.nodes()] 
         for ref in self.refs:
-            for v in self.vars:
+            for v in using_vars:
                 try:
                     tgt = str(self.eval(ref(v)))
                     if tgt in all_nodes_str:
                         self.G.add_edge(str(v), tgt)
                 except Z3Exception:
                     None
-        
         pos=nx.spring_layout(self.G)
-        label = dict((n, str(n)+':'+self._typeOf(n) + self._attr(n)) for n in self.G.nodes()) 
+        label = dict((n, str(n)+':'+d['type'] + self._attr(n)) for n,d in self.G.nodes(data=True)) 
         plt.ion()
-        nx.draw(self.G, pos=pos, with_lables=True, node_size=5000)
+        
+        node_type_dict = dict()
+        for n,d in self.G.nodes(data=True):
+            t = d['type']
+            if t in node_type_dict:
+                node_type_dict[t].append(n)
+            else:
+                node_type_dict[t] = [n]
+        ci = 0
+        for t, ns in node_type_dict.items():
+            nx.draw_networkx_nodes(self.G, pos, nodelist= ns, node_size = 5000, node_color = self._colors[ci])
+            ci = ci+1
+        #nx.draw(self.G, pos=pos, with_lables=True, node_size=5000)
         nx.draw_networkx_labels(self.G, pos, label)
+        nx.draw_networkx_edges(self.G, pos, self.G.edges())
         plt.show(block=True)   
     
     def _filt(self, v):  
@@ -47,17 +66,19 @@ class ResultPainter():
             return True 
         if not self.use_filters: 
             return True
-        keep = False
         for filter in self.filters:
             try:
                 tgt = str(self.eval(filter(v)))
-                if tgt == 'True': keep = True
+                if tgt == 'True': return True
             except:
                 None
-        return keep
+        return False
     
-    def _typeOf(self, vstr):
+    def _type_of(self, vstr):
         v = next(n for n in self.vars if str(n)==vstr)
+        return self._type_of_v(v)
+    
+    def _type_of_v(self,v):
         for t in self.types:
             try:
                 typestr = str(self.eval(t(v)))
@@ -65,6 +86,17 @@ class ResultPainter():
             except:
                 None
         return ""
+    
+    def _attr_of_v(self,v):
+        ret = dict()
+        for a in self.attrs:
+            try:
+                attrstr = str(self.eval(a(v)))
+                if not('(' in attrstr):
+                    ret[str(a)] = attrstr
+            except:
+                None
+        return ret
     
     def _attr(self, vstr):
         v = next(n for n in self.vars if str(n)==vstr)
@@ -77,3 +109,19 @@ class ResultPainter():
             except:
                 None
         return ret
+    
+    def _shannon(self):
+        pop = dict()
+        for n, d in self.G.nodes(data = True):
+            type = str(d['type'])
+            if type in pop:
+                pop[type] = pop[type] + 1
+            else:
+                pop[type] = 1
+        n = float(len(pop))
+        p = [x / n for k,x in pop.items()]
+        shannon = 0 - sum(pi * math.log(pi) for pi in p)
+        return shannon
+            
+            
+            
